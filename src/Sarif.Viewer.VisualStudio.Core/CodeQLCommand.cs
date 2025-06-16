57e8ff6612df4ae776bd0e33c6f05e2a76bf45d7
Jacob Ronstadt <jronstadt@microsoft.com>
2025-06-16 15:32:05 -0700

    fix bug where codeql settings not saving

--- src/Sarif.Viewer.VisualStudio.Core/CodeQLCommand.cs
+++ src/Sarif.Viewer.VisualStudio.Core/CodeQLCommand.cs
@@ -191,11 +191,11 @@ namespace Microsoft.Sarif.Viewer.VisualStudio.Core.CodeQL
                     {
                         dbSuccessful = await CodeQLService.Instance.GenerateCodeQLDatabaseAsync();
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         CodeQLService.Instance.ClearTask();
                         VsShellUtilities.ShowMessageBox(Microsoft.VisualStudio.Shell.ServiceProvider.GlobalProvider,
-                                                        $"CodeQL database create failed. See output for details.",
+                                                        $"CodeQL database create failed. " + ex.Message + " See output for details.",
                                                         null, // title
                                                         OLEMSGICON.OLEMSGICON_CRITICAL,
                                                         OLEMSGBUTTON.OLEMSGBUTTON_OK,
@@ -215,7 +215,7 @@ namespace Microsoft.Sarif.Viewer.VisualStudio.Core.CodeQL
                         {
                             CodeQLService.Instance.ClearTask();
                             VsShellUtilities.ShowMessageBox(Microsoft.VisualStudio.Shell.ServiceProvider.GlobalProvider,
-                                                            $"CodeQL analysis failed. See output for details. " + ex.Message,
+                                                            $"CodeQL analysis failed. " + ex.Message + " See output for details. ",
                                                             null, // title
                                                             OLEMSGICON.OLEMSGICON_CRITICAL,
                                                             OLEMSGBUTTON.OLEMSGBUTTON_OK,
